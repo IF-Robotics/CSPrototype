8ce6139d45e01596f3de0dd2106b93ba8c2c3f25
Fall Gold <92420381+Fall-Gold@users.noreply.github.com>
2023-12-17 22:58:06 -0800

    updated teleop to drive better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -31,12 +31,13 @@ public class Teleop extends LinearOpMode {
 
         claw = hardwareMap.get(Servo.class, "claw");
         wrist = hardwareMap.get(Servo.class, "wrist");
-        int position = 0;
+        int position = 140;
 
         BR.setDirection(DcMotorSimple.Direction.REVERSE);
         FR.setDirection(DcMotorSimple.Direction.REVERSE);
-        arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        claw.setPosition(.40);
 
         waitForStart();
         arm.setTargetPosition(0);
@@ -45,14 +46,38 @@ public class Teleop extends LinearOpMode {
         while(opModeIsActive()) {
 
             //drive code
-            double speed = 1;
-            BL.setPower(speed * (gamepad1.left_stick_y + gamepad1.left_stick_x));
+            double speed = - .2;
+
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            FL.setPower(speed * frontLeftPower);
+            BL.setPower(speed * backLeftPower);
+            FR.setPower(speed * frontRightPower);
+            BR.setPower(speed * backRightPower);
+
+            /*BL.setPower(speed * (gamepad1.left_stick_y + gamepad1.left_stick_x));
             BR.setPower(speed * (gamepad1.right_stick_y - gamepad1.left_stick_x));
             FL.setPower(speed * (gamepad1.left_stick_y - gamepad1.left_stick_x));
-            FR.setPower(speed * (gamepad1.right_stick_y + gamepad1.left_stick_x));
+            FR.setPower(speed * (gamepad1.right_stick_y + gamepad1.left_stick_x));*/
 
             //arm
-            position += (int) (2 * gamepad2.left_stick_y);
+            position += (int) (-2 * gamepad2.left_stick_y);
+            if(gamepad1.dpad_up) {
+                position += 2;
+            } else if (gamepad1.dpad_down){
+                position -= 2;
+            }
             arm.setTargetPosition(position);
             arm.setPower(.5);
             telemetry.addData("position", position);
@@ -61,10 +86,10 @@ public class Teleop extends LinearOpMode {
 
             //claw
             // use .5 for full open, .4 for medium open, .3 for close on pixel, 0 for total close
-            if (gamepad2.left_bumper) {
-                claw.setPosition(.2);
-            } else if (gamepad2.right_bumper) {
-                claw.setPosition(.36);
+            if (gamepad2.left_bumper || gamepad1.left_bumper) {
+                claw.setPosition(.23);
+            } else if (gamepad2.right_bumper || gamepad1.right_bumper) {
+                claw.setPosition(.40);
             }
 
             //wrist
